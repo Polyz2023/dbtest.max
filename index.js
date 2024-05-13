06e7af3a1cc88bb6b21ba364bddccb77c342edd6
@Programer11 <151386988+Polyz2023@users.noreply.github.com>
2024-05-13 20:45:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 5146;
-
-const { connectToDb, getDb } = require('./config/db')
-
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5146;
+const URL = 'mongodb+srv://nodeprogramer:F4by5!f5RU7YJ25@cluster0.odjfrd7.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0';
 
+let dbConnection;
+
+// Подключение к базе данных
+MongoClient.connect(URL)
+    .then((client) => {
+        console.log('Connected to MongoDB');
+        dbConnection = client.db();
+        
+        // Запуск сервера
+        app.listen(port, () => {
+            console.log("Server is running on port", port);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+// Парсинг JSON-тела запросов
 app.use(bodyParser.json()); 
 
-let db;
-
-connectToDb((err) => {
-    if (!err){
-        app.listen(port, ()=>{
-            console.log("all good");
-        });
-        db= getDb();
-    }else{
-        console.log(`loshara: ${err}`);
-    }
-});
-
+// Обработчик маршрута для получения списка фильмов
 app.get('/', (req, res) => {
-    /*const movies = [];
-
-    db
-    .collection('dbtesto')
-    .find()
-    .forEach(movie => {
-        movies.push(movie);
-    })
-    .then(()=>{
-        res
-        .status(200)
-        .json(movies)
-    })*/
     res.sendFile(__dirname + '/index.html');
 });
 
-
+// Обработчик маршрута для добавления фильма
 app.post('/m', (req, res) => {
     console.log(req.body);
-    db.collection('dbtesto').insertOne(req.body, (err, result) => {
+    dbConnection.collection('dbtesto').insertOne(req.body, (err, result) => {
         if (err) {
-            console.log(err);
+            console.error(err);
             res.status(500).send("Ошибка при добавлении в базу данных");
         } else {
             res.status(200).send("Данные успешно добавлены в базу данных");
         }
     });
 });
-app.get('/m', (req, res) => {console.log(req.body);})
+
+// Обработчик маршрута для получения фильмов (пример)
+app.get('/m', (req, res) => {
+    console.log("GET /m route hit");
+    dbConnection.collection('dbtesto').find().toArray((err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Ошибка при получении данных из базы данных");
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
+
